af16afd0862fdc12e82051fdcc6226e72b076591
t-fan-gif <inotijwork6@gmail.com>
2025-07-05 15:46:14 +0900

    Update GitHub Pages

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"8cd19e509d6bece8ccd74aef027c4ca9473630
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1359314583"
+    serviceWorkerVersion: "3132383801"
   }
 });
